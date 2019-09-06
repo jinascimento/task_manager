41d9c4dac5d29d8c57cbdeb857f3261c9d7f6d16
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-09-06 15:23:17 -0300

    Apply theme bootstrap

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,8 +7,28 @@ Rails.application.config.assets.version = '1.0'
 # Rails.application.config.assets.paths << Emoji.images_path
 # Add Yarn node_modules folder to the asset load path.
 Rails.application.config.assets.paths << Rails.root.join('node_modules')
+Rails.application.config.assets.paths += Dir["#{Rails.root}/vendor/*"]
+Rails.application.config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif *.svg)
 
 # Precompile additional assets.
-# application.js, application.css, and all non-JS/CSS in the app/assets
+# application.js, application.scss, and all non-JS/CSS in the app/assets
 # folder are already added.
-# Rails.application.config.assets.precompile += %w( admin.js admin.css )
+Rails.application.config.assets.precompile += %w(
+  common/common.min.js
+  custom.min.js
+  settings.js
+  gleek.js
+  styleSwitcher.js
+  chart.js/Chart.bundle.min.js
+  circle-progress/circle-progress.min.js
+  d3v3/index.js
+  topojson/topojson.min.js
+  datamaps/datamaps.world.min.js
+  raphael/raphael.min.js
+  morris/morris.min.js
+  moment/moment.min.js
+  pg-calendar/js/pignose.calendar.min.js
+  chartist/js/chartist.min.js
+  chartist-plugin-tooltips/js/chartist-plugin-tooltip.min.js
+  dashboard/dashboard-1
+)
