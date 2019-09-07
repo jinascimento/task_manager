0f3706782d6f48e8dbdb7df011ed4f1295a8b178
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-09-07 19:06:44 -0300

    Fix range date when create task

--- app/assets/javascripts/full_calendar.js
+++ app/assets/javascripts/full_calendar.js
@@ -16,7 +16,12 @@ initialize_calendar = function() {
             events: '/tasks.json',
 
             select: function(start, end) {
-                $.getScript('/tasks/new', function() {});
+                $.getScript('/tasks/new', function() {
+                    $('#event_date_range').val(moment(start).format("MM/DD/YYYY HH:mm") + ' - ' + moment(end).format("MM/DD/YYYY HH:mm"))
+                    date_range_picker();
+                    $('.start_hidden').val(moment(start).format('YYYY-MM-DD HH:mm'));
+                    $('.end_hidden').val(moment(end).format('YYYY-MM-DD HH:mm'));
+                });
 
                 calendar.fullCalendar('unselect');
             },
@@ -38,7 +43,12 @@ initialize_calendar = function() {
             },
 
             eventClick: function(task, jsEvent, view) {
-                $.getScript(task.edit_url, function() {});
+                $.getScript(task.edit_url, function() {
+                    $('#event_date_range').val(moment(task.start).format("MM/DD/YYYY HH:mm") + ' - ' + moment(task.end).format("MM/DD/YYYY HH:mm"))
+                    date_range_picker();
+                    $('.start_hidden').val(moment(task.start).format('YYYY-MM-DD HH:mm'));
+                    $('.end_hidden').val(moment(task.end).format('YYYY-MM-DD HH:mm'));
+                });
             }
         });
     })
