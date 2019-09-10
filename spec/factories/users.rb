a11cce86b7e0771d522a9ebb40b6daeb2147f265
Jhonatan I <jhonatan-nascimento94@hotmail.com>
2019-09-10 19:00:09 -0300

    Add user spec

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,8 @@
 FactoryBot.define do
   factory :user do
-    
+    email { 'teste@teste.com.br' }
+    password { 123456 }
+    password_confirmation { 123456 }
+    person
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe 'validates' do
+    it "is not valid without a required field" do
+      user = User.new
+      expect(user).to_not be_valid
+    end
+
+    it "is valid with all required fields" do
+      user = create(:user)
+      expect(user).to be_valid
+    end
+  end
+
+  describe 'associations' do
+    it 'belongs_to person' do
+      user = create(:user)
+      expect(user.person).to_not be_nil
+    end
+  end
 end
